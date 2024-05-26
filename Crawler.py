65c2bbe15508fa718ad6d54ff29dc37e0a3297ea
kangdaeki <dkkang@gmail.com>
2024-05-26 19:37:41 +0900

    Test scrapy

--- Crawler.py
+++ Crawler.py
@@ -1,9 +1,24 @@
-#!/usr/bin/env python
+#!/Users/user/AppData/Local/Microsoft/WindowsApps/python3.exe
 # -*- coding: utf-8 -*-
-print("Hello")
 
+import scrapy
 
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = ["https://quotes.toscrape.com/tag/humor/",]
 
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                "author": quote.xpath("span/small/text()").get(),
+                "text": quote.css("span.text::text").get(),
+            }
 
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
 
+"""
+#####!/usr/bin/env python
+"""
 
